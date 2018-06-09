62c0d347a459c30d37f24630da980bdd78b828fb
Ovv <contact@ovv.wtf>
2018-06-09 22:31:27 +0200

    flake8

--- isitdown/checks/__init__.py
+++ isitdown/checks/__init__.py
@@ -1,3 +1,4 @@
+# flake8: noqa
 from .base import Result, BaseChecks
 from .http import BaseHTTPCheck, StatusCodeHTTPCheck
 from .ssh import BaseSSHCommandCheck, ExitCodeSSHCommandCheck, StdoutSSHCommandCheck
